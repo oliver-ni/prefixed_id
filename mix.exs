e5dc6165c64079fb117185a282d4f41120ae3c53
Oliver Ni <oliverni@berkeley.edu>
2024-07-06 01:49:42 -0400

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule PrefixedID.MixProject do
   def project do
     [
       app: :prefixed_id,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       description: "UUIDv7-backed human-readable IDs for Ecto",
