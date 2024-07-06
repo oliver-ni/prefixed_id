8142f016ddf05c18eec36315e2054755dd1f9cba
Oliver Ni <oliverni@berkeley.edu>
2024-07-05 21:46:48 -0400

    fix: refactor and fix issues

--- lib/prefixed_uuid.ex
+++ lib/prefixed_uuid.ex
@@ -17,72 +17,70 @@ defmodule PrefixedUUID do
 
   @impl true
   def init(opts) do
-    prefix =
-      if opts[:primary_key] do
-        Keyword.get(opts, :prefix) || raise "The :prefix option is required for primary keys."
-      else
-        field = Keyword.fetch!(opts, :field)
-        schema = Keyword.fetch!(opts, :schema)
-        %{related: related, related_key: related_key} = schema.__schema__(:association, field)
-        {:parameterized, __MODULE__, %{prefix: prefix}} = related.__schema__(:type, related_key)
-        prefix
-      end
+    field = Keyword.fetch!(opts, :field)
+    schema = Keyword.fetch!(opts, :schema)
 
-    if String.contains?(prefix, "_") do
-      raise "The prefix must not contain an underscore."
-    end
-
-    %{prefix: prefix}
-  end
-
-  @impl true
-  def cast(<<_::288>> = hex_uuid, params) do
-    Ecto.UUID.cast(hex_uuid)
-    |> cast(params)
-  end
-
-  @impl true
-  def cast(<<_::128>> = raw_uuid, params) do
-    load(raw_uuid, nil, params)
-  end
-
-  @impl true
-  def cast(value, %{prefix: prefix}) do
-    with [^prefix, base62_uuid] <- String.split(value, "_"),
-         {:ok, _} <- Nifs.base62_decode(base62_uuid) do
-      value
-    else
-      _ -> :error
+    cond do
+      opts[:primary_key] -> %{primary_key: true, prefix: get_pkey_prefix(opts)}
+      opts[:foreign_key] -> %{field: field, schema: schema}
+      true -> raise "Must be used as either a primary or foreign key."
     end
   end
 
   @impl true
-  def load(nil, _loader, _params) do
-    nil
+  def cast(nil, _params), do: {:ok, nil}
+  def cast(<<_::288>> = hex_uuid, params), do: Ecto.UUID.cast(hex_uuid) |> cast(params)
+  def cast(<<_::128>> = raw_uuid, params), do: load(raw_uuid, nil, params)
+
+  def cast(value, params) do
+    with {:ok, _} <- to_numeric_uuid(value, params) do
+      {:ok, value}
+    end
   end
 
   @impl true
-  def load(<<uuid::128>>, _loader, %{prefix: prefix}) do
-    prefix <> "_" <> Nifs.base62_encode(uuid)
-  end
+  def load(nil, _loader, _params), do: {:ok, nil}
+  def load(<<uuid::128>>, _loader, params), do: {:ok, from_numeric_uuid(uuid, params)}
 
   @impl true
-  def dump(nil, _dumper, _params) do
-    nil
-  end
+  def dump(nil, _dumper, _params), do: {:ok, nil}
+  def dump(value, _dumper, params), do: to_numeric_uuid(value, params)
 
   @impl true
-  def dump(value, _dumper, %{prefix: prefix}) do
+  def autogenerate(params) do
+    Nifs.generate_numeric_uuidv7()
+    |> from_numeric_uuid(params)
+  end
+
+  defp get_pkey_prefix(opts) do
+    prefix = Keyword.get(opts, :prefix)
+    if is_nil(prefix), do: raise("The :prefix option is required for primary keys.")
+    if String.contains?(prefix, "_"), do: raise("The prefix must not contain an underscore.")
+    prefix
+  end
+
+  defp prefix(%{primary_key: true, prefix: prefix}) do
+    prefix
+  end
+
+  defp prefix(%{field: field, schema: schema}) do
+    %{related: related, related_key: related_key} = schema.__schema__(:association, field)
+    {:parameterized, __MODULE__, %{prefix: prefix}} = related.__schema__(:type, related_key)
+    prefix
+  end
+
+  defp to_numeric_uuid(value, params) do
+    prefix = prefix(params)
+
     with [^prefix, base62_uuid] <- String.split(value, "_"),
          {:ok, uuid} <- Nifs.base62_decode(base62_uuid) do
-      <<uuid::128>>
+      {:ok, <<uuid::128>>}
     else
       _ -> :error
     end
   end
 
-  @impl true
-  def autogenerate(%{prefix: prefix}) do
-    prefix <> "_" <> Nifs.base62_encode(Nifs.generate_numeric_uuidv7())
+  defp from_numeric_uuid(uuid, params) do
+    prefix(params) <> "_" <> Nifs.base62_encode(uuid)
   end
 end
