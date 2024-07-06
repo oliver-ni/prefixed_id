d79f60b81f28a73a73c23fb21b96397a3ebb3841
Oliver Ni <oliverni@berkeley.edu>
2024-07-05 22:11:34 -0400

    feat: update mix.exs for packaging

--- mix.exs
+++ mix.exs
@@ -1,35 +1,48 @@
 defmodule PrefixedID.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/oliver-ni/prefixed_id"
+
   def project do
     [
       app: :prefixed_id,
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      description: "UUIDv7-backed human-readable IDs for Ecto",
       deps: deps(),
-      name: "Prefixed ID",
-      source_url: "https://github.com/oliver-ni/prefixed_id",
-      docs: [
-        main: "readme",
-        extras: ["README.md"]
-      ]
+      package: package(),
+      docs: docs(),
+      source_url: @source_url
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp package do
+    [
+      maintainers: ["Oliver Ni"],
+      licenses: ["MPL-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
   defp deps do
     [
       {:rustler, "~> 0.33.0"},
       {:ecto, "~> 3.11"},
       {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 end
